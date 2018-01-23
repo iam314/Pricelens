3e8ac2b79ae16b75da210f875e720b2a11b7c332
Lyudmil <workwith314@gmail.com>
2018-01-24 01:18:27 +0200

    Mobile upadetes and animated responsive nav

--- js/quotes.js
+++ js/quotes.js
@@ -1,3 +1,14 @@
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
 var slideIndex = 0;
 carousel();
 
@@ -11,4 +22,11 @@ function carousel() {
     if (slideIndex > x.length) {slideIndex = 1}
     x[slideIndex-1].style.display = "block";
     setTimeout(carousel, 5000);
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
 }
